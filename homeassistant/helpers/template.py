f36da18cfa9e4865b78525d94169843a68740c93
G Johansson <goran.johansson@shiftit.se>
2024-06-23 14:47:39 +0000

    Extend comment

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -2121,6 +2121,9 @@ def merge_response(
     """Merge service responses into single list.
 
     Checks that the input is a correct service response:
+    {
+        "entity_id": {str: Any},
+    }
     Returns empty list by default.
     """
     response_items: list[Any] = []
